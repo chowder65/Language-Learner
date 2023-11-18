82155567a27f6d7dccabba835eb80188fbb45739
Christian McCulley <127411172+chowder65@users.noreply.github.com>
2023-11-17 19:16:23 -0700

    fix

--- Back/routes/users.js
+++ Back/routes/users.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/getUsers', userController.getUsers);
+router.get('/getUsers', userController.getUser);
 router.post('/addUser', userController.addUser);
 router.patch('/updateuser', userController.updateUser);
 router.delete('/deleteUser', userController.deleteUser);
