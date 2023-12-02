3c2e7da5c8ce5864d8e106de314f7b2e7d7a2eda
Christian McCulley <127411172+chowder65@users.noreply.github.com>
2023-12-01 19:55:57 -0700

    finished python api need to do front end implementation

--- Python-Backend/WhisperIO/main.py
+++ Python-Backend/WhisperIO/main.py
@@ -1,37 +1,41 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from fastapi import FastAPI
+from thing import loadFile
+import os
 
 app = FastAPI()
 
 @app.post("/convertBinarytoMp3")
 async def root(fileId: str):
+    print(fileId)
     return {
-        "speechTotext": convertToMp3("test")
+        "speechTotext": convertToMp3(fileId)
     }
 
 
 
 
-def convertToMp3(File):
+def convertToMp3(fileId):
     uri = "mongodb+srv://mguest:LingoLounge123@lingolounge.ahioda5.mongodb.net/?retryWrites=true&w=majority"
 
     client = MongoClient(uri, server_api=ServerApi('1'))
 
     try:
-        client.admin.command('ping')
-        print("Pinged your deployment. You successfully connected to MongoDB!")
 
         db = client["LingoLounge"]
         collection = db["AudioFiles"]
 
-        data = collection.find_one({"id": "10"})
+        data = collection.find_one({"id": fileId})
+        print(data)
 
-        output_file = "output_audio.mp3"  # Specify the desired output file name
+        output_file = "output_audio.mp3"
         with open(output_file, "wb") as output:
             output.write(data["audio"])
 
         print("Audio file converted back:", output_file)
+
+        return loadFile(output_file)
 
     except Exception as e:
         print(e)
--- Python-Backend/WhisperIO/thing.py
+++ Python-Backend/WhisperIO/thing.py
@@ -1,6 +1,12 @@
 import whisper
+import os
 
 def loadFile(file):
+    print(file)
     model = whisper.load_model("base")
     result = model.transcribe(file)
     print(result["text"])
+
+    os.remove("output_audio.mp3")
+
+    return result["text"]
