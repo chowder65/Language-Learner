bc66b220bc2c83ce13a4ed385e70d4079c5d54ca
Christian McCulley <127411172+chowder65@users.noreply.github.com>
2023-12-05 20:38:29 -0700

    finished the implementation of the STT to the frontend

--- Python-Backend/WhisperIO/main.py
+++ Python-Backend/WhisperIO/main.py
@@ -1,41 +1,59 @@
+import uvicorn
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from fastapi import FastAPI
 from thing import loadFile
+from fastapi.middleware.cors import CORSMiddleware
+from translate import Translator
 import os
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+translator = Translator(to_lang="en", from_lang="es")
+
 @app.post("/convertBinarytoMp3")
 async def root(fileId: str):
-    print(fileId)
+    print("fileId: ", fileId)
+    speechTotext = await convertToMp3(fileId)
+    print(speechTotext)
     return {
-        "speechTotext": convertToMp3(fileId)
+        "speechTotext": speechTotext
     }
 
 
-def convertToMp3(fileId):
-    uri = "mongodb+srv://mguest:LingoLounge123@lingolounge.ahioda5.mongodb.net/?retryWrites=true&w=majority"
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
-    client = MongoClient(uri, server_api=ServerApi('1'))
+async def convertToMp3(fileId):
+    print("fileId: ", fileId)
 
     try:
+        uri = "mongodb+srv://mguest:LingoLounge123@lingolounge.ahioda5.mongodb.net/?retryWrites=true&w=majority"
+
+        client = MongoClient(uri, server_api=ServerApi('1'))
 
         db = client["LingoLounge"]
         collection = db["AudioFiles"]
 
         data = collection.find_one({"id": fileId})
-        print(data)
 
         output_file = "output_audio.mp3"
         with open(output_file, "wb") as output:
             output.write(data["audio"])
 
-        print("Audio file converted back:", output_file)
+        translation = translator.translate(loadFile(output_file))
+        print(translation)
 
-        return loadFile(output_file)
-
+        return translation
     except Exception as e:
         print(e)
-
-convertToMp3("test")
+        return "Error"
+    
--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cons = require('consolidate');
 var session = require('express-session')
+const cors = require('cors');
 
 var indexRouter = require('./Back/routes/index');
 var usersRouter = require('./Back/routes/users');
@@ -25,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 app.use(session({
   secret: 'UserLoginSession', //can be anything, needs to be secure and realted to project
   resave: false,
